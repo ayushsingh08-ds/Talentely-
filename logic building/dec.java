d695c17c19da13b2472703af2613cc645c98c165
ASH <ayushsinghd49@gmail.com>
2025-09-17 12:11:26 +0530

    dectobin

---
+++ logic building/dec.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class dec {
+    static String dectoBinary(int n) {
+        StringBuilder bin = new StringBuilder();
+        while (n > 0) {
+            int bit = n & 1;
+            bin.append(bit);
+            n = n >> 1;
+        }
+        return bin.reverse().toString();
+    }
+
+    public static void main(String[] args) {
+        int n = 12;
+        System.out.println(dectoBinary(n));
+
+    }
+}
